1b726c0e4bf3a36e6a53f55b78d4abb12519d043
= <peterndirangu0005@gmail.com>
2023-11-01 21:35:30 +0300

    update

---
+++ server/models/task.py
@@ -0,0 +1,7 @@
+from server import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
---
+++ server/routes/task_routes.py
@@ -0,0 +1,38 @@
+from models.task import Task
+from flask import request, jsonify
+from server.app import app, db
+from server.models.task import Task
+
+@app.route('/api/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(title=data['title'], description=data['description'], user_id=data['user_id'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task created successfully!'}), 201
+
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([task.serialize for task in tasks]), 200
+
+@app.route('/api/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    return jsonify(task.serialize), 200
+
+@app.route('/api/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    data = request.get_json()
+    task = Task.query.get_or_404(task_id)
+    task.title = data['title']
+    task.description = data['description']
+    db.session.commit()
+    return jsonify({'message': 'Task updated successfully!'}), 200
+
+@app.route('/api/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Task deleted successfully!'}), 200
