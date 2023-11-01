601ebaf29db86bbdf4763e2cfcf2fd313faa98b2
Gideon <gideonnemugere@gmail.com>
2023-11-01 20:15:38 +0300

    Updated Asssignment

--- Client/todo-list-app/src/App.js
+++ Client/todo-list-app/src/App.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from './components/login.component'
-import SignUp from './components/signup.component'
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './components/login.component';
+import SignUp from './components/signup.component';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
           <div className="container">
             <Link className="navbar-brand" to={'/sign-in'}>
             </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link fancy-button1 mr-2" to={'/sign-in'}>
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fancy-button2" to={'/sign-up'}>
+                  Sign up
+                </Link>
+              </li>
+            </ul>
           </div>
         </nav>
         <div className="auth-wrapper">
@@ -40,6 +39,7 @@ function App() {
         </div>
       </div>
     </Router>
-  )
+  );
 }
-export default App
+
+export default App;
